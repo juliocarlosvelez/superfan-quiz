4dcef2dfd4320668fc2b498e98348561050b0d43
Juliocarlos Velez <juliocarlosvelez@gmail.com>
2018-06-25 18:21:32 +0200

    Main page with decent CSS is working, no gameplay availible yet

--- Game.js
+++ Game.js
@@ -1,6 +1,6 @@
 'use strict'
 
-function main() {
+function Game() {
     var buttonDiv;
     var button;
     var body;
@@ -23,32 +23,80 @@ function main() {
         buttonDiv.appendChild(button);
         body.appendChild(buttonDiv);
     
-        button.addEventListener('click', handleStartClick)
+        button.addEventListener('click', startClick)
     }
     buildSplash();
 
-    function handleStartClick(){
-        button.removeEventListener('click', handleStartClick);
+    function startClick(){
+        button.removeEventListener('click', startClick);
         buttonDiv.remove();
         titleDiv.remove(); 
         body.setAttribute('class', 'hide');
         startGame(button.innerText);
     }
+    var questionDiv;
+    var questionText;
+    var answersDiv;
+    var innerDivA;
+    var innerDivB;
+    var innerDivC;
+    var buttona;
+    var buttonb;
+    var buttonc;
 
     function startGame (nameofartist) { 
         var chosenArtist;
         if (artistObject.artistsArray[5].name === nameofartist) {
             chosenArtist = artistObject.artistsArray[5].songs;
         }
+        body = document.body;
+        questionDiv = document.createElement('div');
+        questionDiv.setAttribute('class', 'question-box paper');
+        questionText = document.createElement('h1');
+        questionText.setAttribute('class','question-text')
+        questionText.innerText= Question(chosenArtist);
+        questionDiv.appendChild(questionText);
+        body.appendChild(questionDiv);
+
+        answersDiv = document.createElement('div');
+        answersDiv.setAttribute('class', 'answer-boxes');
         
-        Question(chosenArtist);
+        innerDivA = document.createElement('div');
+        innerDivA.setAttribute('class', 'answer-a-box');
+        
+        innerDivB = document.createElement('div');
+        innerDivB.setAttribute('class', 'answer-b-box');
+        
+        innerDivC = document.createElement('div');
+        innerDivC.setAttribute('class', 'answer-c-box');
+        
+        buttona = document.createElement('button');
+        buttona.setAttribute('class', 'choice-a');
+        buttona.innerText = 'A';
+
+        buttonb = document.createElement('button');
+        buttonb.setAttribute('class', 'choice-b');
+        buttonb.innerText = 'B';
+
+        buttonc = document.createElement('button');
+        buttonc.setAttribute('class', 'choice-c');
+        buttonc.innerText = 'C';
+
+        innerDivA.appendChild(buttona);
+        innerDivB.appendChild(buttonb);
+        innerDivC.appendChild(buttonc);
+        answersDiv.appendChild(innerDivA);
+        answersDiv.appendChild(innerDivB);
+        answersDiv.appendChild(innerDivC);
+        body.appendChild(answersDiv);
+        body.setAttribute('class', 'game-screen')
     }
 
     function Question(artistChosen) {
-        return ("The song "+ artistChosen[0].songName)
+        
+        return ("The quote:"+ artistChosen[0].songQuote + " is from what "+button.innerText+" song?")
     }
-
 }
 
-window.addEventListener('load', main)
+window.addEventListener('load', Game)
 
